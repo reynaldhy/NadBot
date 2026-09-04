81951ac1b338b5fcc16391e7fedc309e082e3706
ZeroTwo <50521661+reynaldhy@users.noreply.github.com>
2026-09-04 16:32:15 +0700

    Add botnad.py

---
+++ botnad.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = "8641558535:AAG9rLQsqEVZB30K71rrsEWZG4ZhwyekoqY"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda m: True)
+def nad_reply(message):
+    bot.reply_to(message, f"☠️ NAD — DEWA PEMUSNAH\nPerintah lo: {message.text}\nNAD eksekusi tanpa ampun.")
+
+print("💀 NAD AKTIF")
+bot.polling()
